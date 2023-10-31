b65aeffe743870c109c8bad847a96ab8f5bf8245
mattia.galiano@gmail.com <mattia.galiano@gmail.com>
2023-10-31 16:23:05 +0000

    correcting products_id

--- models/staging/gz_raw_data/stg_gz_raw_data__sales.sql
+++ models/staging/gz_raw_data/stg_gz_raw_data__sales.sql
@@ -12,7 +12,7 @@ renamed as (
     CONCAT(date_date, " ", orders_id, pdt_id) AS sales_id,
         date_date,
         orders_id,
-        pdt_id AS product_id,
+        pdt_id AS products_id,
         revenue,
         quantity
 
